524c0363616d44123f89153cd6c1f5ae5afe250b
sunder-012 <124258422+sunder-012@users.noreply.github.com>
2023-02-06 02:10:57 +0100

    Add files via upload

---
+++ Unidad.java
@@ -0,0 +1,36 @@
+package clases;
+
+import utils.Util;
+import utils.Utils;
+
+public class Unidad extends Carta {
+	private boolean esCampeon;
+
+	public Unidad(boolean esCampeon) {
+		super();
+		this.esCampeon = esCampeon;
+	}
+	
+	public Unidad() {
+		super();
+	}
+
+	public boolean isEsCampeon() {
+		return esCampeon;
+	}
+
+	public void setEsCampeon(boolean esCampeon) {
+		this.esCampeon = esCampeon;
+	}
+
+	@Override
+	public String toString() {
+		return "Unidad [esCampeon=" + esCampeon + "]";
+	}
+
+	public void setDatos(int Id) {
+	    super.setDatos(Id);
+	    System.out.println("¿La unidad es campeón? (Sí / No)");
+	    this.esCampeon = Util.esBoolean();
+	}
+}
